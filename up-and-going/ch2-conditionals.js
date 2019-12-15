ea34db9385877b9bde8ce38756722f781cb8ddc8
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-12-14 20:19:18 -0700

    conditionals

--- up-and-going/ch2-conditionals.js
+++ up-and-going/ch2-conditionals.js
@@ -26,3 +26,26 @@ switch (a) {
   default:
     // fallback to here     
 }
+
+// The break is important if you want only the statement(s)
+// in one case to run. If you omit the break from a case, and that 
+// case matches or runs, execution will continue with the next case's
+// statements regardless of that case matching.
+
+// "fallthrough" switch
+switch (a) {
+  case 2:
+  case 10:
+    // some cool stuff
+    break;
+  case 42:
+    // other stuff
+    break;
+  default:
+    // fallback    
+}
+
+// ternary operator
+const a = 42;
+
+const b = (a > 41) ? "hello" : "world";
